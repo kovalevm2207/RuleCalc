63424b13e592d19aa474ea3212e58b5d14a5e845
kovalevm2207 <kovalevm220741@gmail.com>
2025-12-04 02:26:09 +0300

    added func

--- RuleCalculator.cpp
+++ RuleCalculator.cpp
@@ -75,18 +75,43 @@ double GetS(const char** s)
     assert(*s);
 
     SkipSpaces(s);
-    double val1 = GetP(s);
+    double val1 = GetF(s);
     SkipSpaces(s);
 
     if(**s == '^')
     {
         (*s)++;
-        double val2 = GetP(s);
+        double val2 = GetF(s);
 
         val1 = pow(val1, val2);
     }
 
     return val1;
+}
+double GetF(const char** s)
+{
+    assert(s);
+    assert(*s);
+
+    SkipSpaces(s);
+
+    char* word = GetW(s);
+    double val = GetP(s);
+
+    if(word)
+    {
+        for(size_t i = 0; i < FUNC_NUM; i++)
+        {
+            if(strcmp(word, func[i].name) == 0)
+            {
+                return func[i].action(val);
+            }
+        }
+        ERR_PRINT("SyntaxErr\n");
+        return 0;
+    }
+
+    return val;
 }
 double GetP(const char** s)
 {
@@ -174,6 +199,49 @@ double GetN(const char** s)
     ON_DEBUG_PRINT("OUT GetN\n");
     return val * sign; // TreeNodeCtor_(NUM, {.num = val}, NULL, NULL)
 }
+char*  GetW(const char** s)
+{
+    assert(s);
+    assert(*s);
+
+    char* word = (char*) calloc(ST_W_LEN, sizeof(char));
+    assert(word && "mem alloc err (calloc)");
+
+    size_t letter = 0;
+    size_t word_len = ST_W_LEN;
+
+    SkipSpaces(s);
+    while(('A' <= (**s) && (**s) <= 'Z') ||
+          ('a' <= (**s) && (**s) <= 'z'))
+    {
+        word[letter++] = **s;
+        (*s)++;
+
+        word_len = CheckLen(&word, word_len, letter);
+        assert(word_len);
+    }
+    if(letter == 0)
+    {
+        free(word);
+        return NULL;
+    }
+
+    while(('A' <= (**s) && (**s) <= 'Z') ||
+          ('a' <= (**s) && (**s) <= 'z') ||
+          ('0' <= (**s) && (**s) <= '9') ||
+          (**s) == '_')
+    {
+        word[letter++] = **s;
+        (*s)++;
+
+        word_len = CheckLen(&word, word_len, letter);
+        assert(word_len);
+    }
+
+    word[letter] = '\0';
+
+    return word;
+}
 double SkipSpaces(const char** s)
 {
     assert(s);
@@ -182,4 +250,29 @@ double SkipSpaces(const char** s)
     while(isspace(**s)) ++*s;
 
     return 0;
+}
+size_t CheckLen(char** word, size_t word_len, const size_t letter)
+{
+    assert(word);
+    assert(*word);
+    assert(letter <= word_len);
+
+    if(letter == word_len)
+    {
+        char* new_word = (char*) realloc(*word, (word_len * 2) * sizeof(char));
+        assert(new_word);
+        *word = new_word;
+        memset(*word + word_len, 0, word_len);
+        word_len *= 2;
+    }
+
+    return word_len;
+}
+double cotan(double val)
+{
+    return 1/tan(val);
+}
+double ctanh(double val)
+{
+    return 1/tanh(val);
 }
--- RuleCalculator.h
+++ RuleCalculator.h
@@ -6,6 +6,7 @@
 #include <stdlib.h>
 #include <ctype.h>
 #include <math.h>
+#include <string.h>
 
 #include "color_print.h"
 
@@ -18,13 +19,43 @@
 #define ERR_PRINT(text) fprintf(stderr, RED_COLOR text RESET)
 
 const size_t ST_EXP_LEN = 5;
+const size_t ST_W_LEN = 5;
+
 
 double GetG(const char** s);
 double GetE(const char** s);
 double GetT(const char** s);
 double GetS(const char** s);
+double GetF(const char** s);
 double GetP(const char** s);
 double GetN(const char** s);
+char*  GetW(const char** s);
 double SkipSpaces(const char** s);
+size_t CheckLen(char** word, size_t word_len, const size_t letter);
+double cotan(double val);
+double ctanh(double val);
+
+const struct
+{
+    const char* const name;
+    double (*action)(double val);
+} func[] ={
+    {"sin",   sin},
+    {"cos",   cos},
+    {"tan",   tan},
+    {"ctan",  cotan},
+    {"ln",    log},
+    {"lg",    log10},
+    {"sqrt",  sqrt},
+    {"sinh",  sinh},
+    {"cosh",  cosh},
+    {"tanh",  tanh},
+    {"ctanh", ctanh},
+    {"asin",  asin},
+    {"acos",  acos},
+    {"atan",  atan}
+};
+
+const size_t FUNC_NUM = sizeof(func)/sizeof(func[0]);
 
 #endif  // RULE_CALC_MAIN_H
